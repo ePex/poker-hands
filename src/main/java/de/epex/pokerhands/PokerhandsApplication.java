ff91a2ebbeafc140b0b9a7c7a7eed55a707ac7b7
Philipp Holz <phil.epex@googlemail.com>
2018-03-09 21:39:30 +0100

    initial commit

---
+++ src/main/java/de/epex/pokerhands/PokerhandsApplication.java
@@ -0,0 +1,12 @@
+package de.epex.pokerhands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerhandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerhandsApplication.class, args);
+	}
+}
