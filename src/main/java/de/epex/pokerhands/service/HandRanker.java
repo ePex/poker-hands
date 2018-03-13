4dd45b2bfaf51b6a137a0f7ba8d5699c3e785396
Philipp Holz <phil.epex@googlemail.com>
2018-03-14 00:18:54 +0100

    adds implementation for pair ranking

--- src/main/java/de/epex/pokerhands/service/HandRanker.java
+++ src/main/java/de/epex/pokerhands/service/HandRanker.java
@@ -1,5 +1,8 @@
 package de.epex.pokerhands.service;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class HandRanker {
     public Rank getRank(String hand) {
         String[] cards = hand.split("\\s");
@@ -11,7 +14,23 @@ public class HandRanker {
             }
         }
 
+        int pairCount = getPairCount(cards);
+        if (pairCount == 1) {
+            return Rank.PAIR;
+        }
+
         return Rank.HIGH_CARD;
+    }
+
+    private int getPairCount(String[] cards) {
+        List<Integer> strings = Arrays.stream(cards).map(this::getValue).collect(Collectors.toList());
+
+        Set<Integer> allItems = new HashSet<>();
+        Set<Integer> duplicates = strings.stream()
+                .filter(n -> !allItems.add(n)) //Set.add() returns false if the item was already in the set.
+                .collect(Collectors.toSet());
+
+        return duplicates.size();
     }
 
     private int getValue(String card) {
