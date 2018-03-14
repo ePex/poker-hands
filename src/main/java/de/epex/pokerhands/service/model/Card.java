820dcc03d522e467341cd129ddabad5b67ae1281
Philipp Holz <phil.epex@googlemail.com>
2018-03-14 21:55:42 +0100

    adds card model and test

---
+++ src/main/java/de/epex/pokerhands/service/model/Card.java
@@ -0,0 +1,47 @@
+package de.epex.pokerhands.service.model;
+
+public class Card {
+
+    private final String suite;
+
+    private final int value;
+
+    public Card(String cardString) {
+        suite = getSuiteFromCardString(cardString);
+        value = getValueFromCardString(cardString);
+    }
+
+    private String getSuiteFromCardString(String card) {
+        return String.valueOf(card.charAt(0));
+    }
+
+    private int getValueFromCardString(String card) {
+        String stringValue = card.substring(1).toUpperCase();
+
+        switch (stringValue) {
+            case "J":
+                return 10;
+            case "Q":
+                return 11;
+            case "K":
+                return  12;
+            case "A":
+                return  13;
+            default:
+                return Integer.valueOf(stringValue);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%s", suite, value);
+    }
+
+    public String getSuite() {
+        return suite;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
