d65634e89fec0e625ccbf0e625c7418896551bd1
Philipp Holz <phil.epex@googlemail.com>
2018-03-15 01:57:22 +0100

    changes enum names

--- src/main/java/de/epex/pokerhands/service/Rank.java
+++ src/main/java/de/epex/pokerhands/service/Rank.java
@@ -1,16 +1,16 @@
 package de.epex.pokerhands.service;
 
 public enum Rank {
-    HIGH_CARD(1, "High Card"),
-    PAIR(2, "High Card"),
-    TWO_PAIRS(3, "High Card"),
-    THREE_OF_A_KIND(4, "High Card"),
-    STRAIGHT(5, "High Card"),
-    FLUSH(6, "High Card"),
-    FULL_HOUSE(7, "High Card"),
-    FOUR_OF_A_KIND(8, "High Card"),
-    STRAIGHT_FLUSH(9, "High Card"),
-    ROYAL_FLUSH(10, "High Card");
+    HIGH_CARD(1, "High card"),
+    PAIR(2, "Pair"),
+    TWO_PAIRS(3, "Two pairs"),
+    THREE_OF_A_KIND(4, "Three of a kind"),
+    STRAIGHT(5, "Straight"),
+    FLUSH(6, "Flush"),
+    FULL_HOUSE(7, "Full house"),
+    FOUR_OF_A_KIND(8, "Four of a kind"),
+    STRAIGHT_FLUSH(9, "Straight flush"),
+    ROYAL_FLUSH(10, "Royal flush");
 
     private final int value;
 
