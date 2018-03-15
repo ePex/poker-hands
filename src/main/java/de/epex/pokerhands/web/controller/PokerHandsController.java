cdf07f58479d1f79b57af6180198bfa290d977c2
Philipp Holz <phil.epex@googlemail.com>
2018-03-15 02:14:11 +0100

    marriage between backend and frontend classes \o/ whoop whoop \o/

--- src/main/java/de/epex/pokerhands/web/controller/PokerHandsController.java
+++ src/main/java/de/epex/pokerhands/web/controller/PokerHandsController.java
@@ -1,7 +1,9 @@
 package de.epex.pokerhands.web.controller;
 
+import de.epex.pokerhands.service.Evaluator;
 import de.epex.pokerhands.web.dto.CompareHandsDto;
 import de.epex.pokerhands.web.dto.ComparisonResultDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(value = "/poker-hands")
 public class PokerHandsController {
+
+    private final Evaluator evaluator;
+
+    @Autowired
+    public PokerHandsController(Evaluator evaluator) {
+        this.evaluator = evaluator;
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView displayForm() {
@@ -25,11 +34,16 @@ public class PokerHandsController {
     public ModelAndView compareHands(@ModelAttribute CompareHandsDto compareHandsDto) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("comparison-result");
+
         ComparisonResultDto comparisonResultDto = new ComparisonResultDto();
-        comparisonResultDto.setMessage("42");
+        comparisonResultDto.setMessage(getResultMessage(compareHandsDto));
         modelAndView.addObject(comparisonResultDto);
 
         return modelAndView;
+    }
+
+    private String getResultMessage(CompareHandsDto compareHandsDto) {
+        return evaluator.evaluate(compareHandsDto.getFirstHand(), compareHandsDto.getSecondHand());
     }
 
 }
