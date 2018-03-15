155d2dcc30d4fe7e22b88d50897973c71ebaf935
Philipp Holz <phil.epex@googlemail.com>
2018-03-15 01:02:19 +0100

    adds deck size test

--- src/test/java/de/epex/pokerhands/service/model/DeckTest.java
+++ src/test/java/de/epex/pokerhands/service/model/DeckTest.java
@@ -2,11 +2,20 @@ package de.epex.pokerhands.service.model;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 public class DeckTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testIsInDeck() {
         Deck.isInDeck(new Card("S1"));
+    }
+
+    @Test
+    public void testDeckSizeIs52() {
+        assertThat(Deck.getDeckSize(), is(equalTo(52)));
     }
 
 }
