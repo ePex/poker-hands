def2b813d464c36c0983ef7bd8797d9a0d987b66
Philipp Holz <phil.epex@googlemail.com>
2018-03-22 21:39:16 +0100

    adds straight flush draw test

--- src/test/java/de/epex/pokerhands/service/ComparatorTest.java
+++ src/test/java/de/epex/pokerhands/service/ComparatorTest.java
@@ -192,4 +192,16 @@ public class ComparatorTest {
         assertThat(result, is(equalTo(secondHand)));
     }
 
+    @Test
+    public void testStraightFlushDraw() {
+        Hand firstHand = new Hand("D3 D4 D5 D6 D7");
+        Hand secondHand = new Hand("S5 S6 S7 S8 S9");
+
+        Hand result = classUnderTest.compare(firstHand, secondHand);
+        assertThat(result, is(equalTo(secondHand)));
+
+        result = classUnderTest.compare(secondHand, firstHand);
+        assertThat(result, is(equalTo(secondHand)));
+    }
+
 }
