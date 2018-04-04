5d76b75a567bcf6341b63b22526a40e3d3eae380
Philipp Holz <phil.epex@googlemail.com>
2018-04-04 16:17:56 +0200

    adds rest support

---
+++ src/main/java/de/epex/pokerhands/web/controller/PokerHandsRestController.java
@@ -0,0 +1,35 @@
+package de.epex.pokerhands.web.controller;
+
+import de.epex.pokerhands.service.Evaluator;
+import de.epex.pokerhands.web.dto.CompareHandsDto;
+import de.epex.pokerhands.web.dto.ComparisonResultDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/rest/poker-hands")
+public class PokerHandsRestController {
+
+    private final Evaluator evaluator;
+
+    @Autowired
+    public PokerHandsRestController(Evaluator evaluator) {
+        this.evaluator = evaluator;
+    }
+
+    @RequestMapping(value = "/compare-hands", method = RequestMethod.POST)
+    public ComparisonResultDto compareHands(@RequestBody CompareHandsDto compareHandsDto) {
+        ComparisonResultDto comparisonResultDto = new ComparisonResultDto();
+        comparisonResultDto.setMessage(getResultMessage(compareHandsDto));
+
+        return comparisonResultDto;
+    }
+
+    private String getResultMessage(CompareHandsDto compareHandsDto) {
+        return evaluator.evaluate(compareHandsDto.getFirstHand(), compareHandsDto.getSecondHand());
+    }
+
+}
